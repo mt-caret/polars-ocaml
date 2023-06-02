85b04a72cd8a35373ea3f443867d31486c5bfa5c
Masayuki Takeda <mtakeda@oeconomia.jp>
2023-06-02 14:51:55 +0000

    sweeksify and add many more basic expression operators

--- lib/polars_ocaml.ml
+++ lib/polars_ocaml.ml
@@ -1,73 +1,183 @@
 open! Core
 
-external rust_twice : int -> int = "rust_twice"
+module Expr = struct
+  type t
 
-type series
+  external col : string -> t = "rust_expr_col"
+  external int : int -> t = "rust_expr_int"
+  external float : float -> t = "rust_expr_float"
+  external sort : t -> descending:bool -> t = "rust_expr_sort"
+  external head : t -> length:int option -> t = "rust_expr_head"
+  external filter : t -> predicate:t -> t = "rust_expr_filter"
+  external sum : t -> t = "rust_expr_sum"
+  external alias : t -> name:string -> t = "rust_expr_alias"
+  external equal : t -> t -> t = "rust_expr_eq"
 
-external rust_series_new : string -> int list -> series = "rust_series_new"
-external rust_series_to_string_hum : series -> string = "rust_series_to_string_hum"
+  let ( = ) = equal
 
-type lazy_frame
+  external ( <> ) : t -> t -> t = "rust_expr_neq"
+  external ( > ) : t -> t -> t = "rust_expr_gt"
+  external ( >= ) : t -> t -> t = "rust_expr_gt_eq"
+  external ( < ) : t -> t -> t = "rust_expr_lt"
+  external ( <= ) : t -> t -> t = "rust_expr_lt_eq"
+  external not : t -> t = "rust_expr_not"
 
-external rust_lazy_frame_of_parquet
-  :  string
-  -> (lazy_frame, string) result
-  = "rust_lazy_frame_of_parquet"
+  let ( ! ) = not
 
-external rust_lazy_frame_to_dot
-  :  lazy_frame
-  -> (string, string) result
-  = "rust_lazy_frame_to_dot"
+  external and_ : t -> t -> t = "rust_expr_and"
+  external or_ : t -> t -> t = "rust_expr_or"
+  external xor : t -> t -> t = "rust_expr_xor"
 
-type data_frame
+  let ( && ) = and_
+  let ( || ) = or_
+  let ( lxor ) = xor
 
-external rust_data_frame_new
-  :  series list
-  -> (data_frame, string) result
-  = "rust_data_frame_new"
+  external add : t -> t -> t = "rust_expr_add"
+  external sub : t -> t -> t = "rust_expr_sub"
+  external mul : t -> t -> t = "rust_expr_mul"
+  external div : t -> t -> t = "rust_expr_div"
 
-external rust_lazy_frame_to_data_frame
-  :  lazy_frame
-  -> (data_frame, string) result
-  = "rust_lazy_frame_to_data_frame"
+  let ( + ) = add
+  let ( - ) = sub
+  let ( * ) = mul
+  let ( / ) = div
+end
 
-external rust_data_frame_to_string_hum
-  :  data_frame
-  -> string
-  = "rust_data_frame_to_string_hum"
+module Series = struct
+  type t
 
-let%expect_test "twice" =
-  print_s [%sexp (rust_twice 2 : int)];
-  [%expect {| 4 |}];
-  print_s [%sexp (rust_twice Int.max_value : int)];
-  [%expect {| -2 |}];
-  (* TODO: what's an ergonomic way to create series and dataframes easily? *)
-  let series = rust_series_new "col" [ 1; 2; 3 ] in
-  rust_series_to_string_hum series |> print_endline;
-  [%expect {|
-    shape: (3,)
-    Series: 'col' [i64]
-    [
-    	1
-    	2
-    	3
-    ] |}];
+  external int : string -> int list -> t = "rust_series_new_int"
+  external int_option : string -> int option list -> t = "rust_series_new_int_option"
+  external float : string -> float list -> t = "rust_series_new_float"
+
+  external float_option
+    :  string
+    -> float option list
+    -> t
+    = "rust_series_new_float_option"
+
+  external string : string -> string list -> t = "rust_series_new_string"
+
+  external string_option
+    :  string
+    -> string option list
+    -> t
+    = "rust_series_new_string_option"
+
+  external to_string_hum : t -> string = "rust_series_to_string_hum"
+
+  let print t = print_endline (to_string_hum t)
+end
+
+module Data_frame = struct
+  type t
+
+  external create : Series.t list -> (t, string) result = "rust_data_frame_new"
+
+  let create_exn series =
+    create series |> Result.map_error ~f:Error.of_string |> Or_error.ok_exn
+  ;;
+
+  external to_string_hum : t -> string = "rust_data_frame_to_string_hum"
+
+  let print t = print_endline (to_string_hum t)
+end
+
+module Lazy_frame = struct
+  type t
+
+  external scan_parquet : string -> (t, string) result = "rust_lazy_frame_scan_parquet"
+  external of_data_frame : Data_frame.t -> t = "rust_data_frame_lazy"
+  external to_dot : t -> (string, string) result = "rust_lazy_frame_to_dot"
+  external collect : t -> (Data_frame.t, string) result = "rust_lazy_frame_collect"
+
+  let collect_exn t = collect t |> Result.map_error ~f:Error.of_string |> Or_error.ok_exn
+
+  external select : t -> exprs:Expr.t list -> t = "rust_lazy_frame_select"
+end
+
+(* TODO: what's an ergonomic way to create series and dataframes easily? *)
+let%expect_test "examples" =
+  let r = Random.State.make [||] in
   let df =
-    rust_data_frame_new [ series ]
-    |> Result.map_error ~f:(fun error_message -> Exn.create_s [%message error_message])
-    |> Result.ok_exn
+    Data_frame.create_exn
+      [ Series.int_option "nrs" [ Some 1; Some 2; Some 3; None; Some 5 ]
+      ; Series.string_option
+          "names"
+          [ Some "foo"; Some "ham"; Some "spam"; Some "egg"; None ]
+      ; Series.float "random" (List.init 5 ~f:(fun _ -> Random.State.float r 5.))
+      ; Series.string "groups" [ "A"; "A"; "B"; "C"; "B" ]
+      ]
   in
-  rust_data_frame_to_string_hum df |> print_endline;
+  Data_frame.print df;
   [%expect
     {|
-    shape: (3, 1)
-    ┌─────┐
-    │ col │
-    │ --- │
-    │ i64 │
-    ╞═════╡
-    │ 1   │
-    │ 2   │
-    │ 3   │
-    └─────┘ |}]
+    shape: (5, 4)
+    ┌──────┬───────┬──────────┬────────┐
+    │ nrs  ┆ names ┆ random   ┆ groups │
+    │ ---  ┆ ---   ┆ ---      ┆ ---    │
+    │ i64  ┆ str   ┆ f64      ┆ str    │
+    ╞══════╪═══════╪══════════╪════════╡
+    │ 1    ┆ foo   ┆ 1.848939 ┆ A      │
+    │ 2    ┆ ham   ┆ 4.490401 ┆ A      │
+    │ 3    ┆ spam  ┆ 3.147566 ┆ B      │
+    │ null ┆ egg   ┆ 0.156988 ┆ C      │
+    │ 5    ┆ null  ┆ 0.831802 ┆ B      │
+    └──────┴───────┴──────────┴────────┘ |}];
+  let df_numerical =
+    Lazy_frame.of_data_frame df
+    |> Lazy_frame.select
+         ~exprs:
+           Expr.
+             [ col "nrs" + int 5 |> alias ~name:"nrs + 5"
+             ; col "nrs" - int 5 |> alias ~name:"nrs - 5"
+             ; col "nrs" * col "random" |> alias ~name:"nrs * random"
+             ; col "nrs" / col "random" |> alias ~name:"nrs / random"
+             ]
+    |> Lazy_frame.collect_exn
+  in
+  Data_frame.print df_numerical;
+  [%expect
+    {|
+    shape: (5, 4)
+    ┌─────────┬─────────┬──────────────┬──────────────┐
+    │ nrs + 5 ┆ nrs - 5 ┆ nrs * random ┆ nrs / random │
+    │ ---     ┆ ---     ┆ ---          ┆ ---          │
+    │ i64     ┆ i64     ┆ f64          ┆ f64          │
+    ╞═════════╪═════════╪══════════════╪══════════════╡
+    │ 6       ┆ -4      ┆ 1.848939     ┆ 0.540851     │
+    │ 7       ┆ -3      ┆ 8.980802     ┆ 0.445394     │
+    │ 8       ┆ -2      ┆ 9.442697     ┆ 0.953117     │
+    │ null    ┆ null    ┆ null         ┆ null         │
+    │ 10      ┆ 0       ┆ 4.159012     ┆ 6.011044     │
+    └─────────┴─────────┴──────────────┴──────────────┘ |}];
+  let df_logical =
+    Lazy_frame.of_data_frame df
+    |> Lazy_frame.select
+         ~exprs:
+           Expr.
+             [ col "nrs" > int 1 |> alias ~name:"nrs > 1"
+             ; col "random" <= float 0.5 |> alias ~name:"random <= 0.5"
+             ; col "nrs" <> int 1 |> alias ~name:"nrs != 1"
+             ; col "nrs" = int 1 |> alias ~name:"nrs == 1"
+             ; (col "random" <= float 0.5 && col "nrs" > int 1) |> alias ~name:"and_expr"
+             ; (col "random" <= float 0.5 || col "nrs" > int 1) |> alias ~name:"or_expr"
+             ]
+    |> Lazy_frame.collect_exn
+  in
+  Data_frame.print df_logical;
+  [%expect
+    {|
+    shape: (5, 6)
+    ┌─────────┬───────────────┬──────────┬──────────┬──────────┬─────────┐
+    │ nrs > 1 ┆ random <= 0.5 ┆ nrs != 1 ┆ nrs == 1 ┆ and_expr ┆ or_expr │
+    │ ---     ┆ ---           ┆ ---      ┆ ---      ┆ ---      ┆ ---     │
+    │ bool    ┆ bool          ┆ bool     ┆ bool     ┆ bool     ┆ bool    │
+    ╞═════════╪═══════════════╪══════════╪══════════╪══════════╪═════════╡
+    │ false   ┆ false         ┆ false    ┆ true     ┆ false    ┆ false   │
+    │ true    ┆ false         ┆ true     ┆ false    ┆ false    ┆ true    │
+    │ true    ┆ false         ┆ true     ┆ false    ┆ false    ┆ true    │
+    │ null    ┆ true          ┆ null     ┆ null     ┆ null     ┆ true    │
+    │ true    ┆ false         ┆ true     ┆ false    ┆ false    ┆ true    │
+    └─────────┴───────────────┴──────────┴──────────┴──────────┴─────────┘ |}]
 ;;
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,25 +1,201 @@
 use ocaml_interop::{
-    ocaml_export, BoxRoot, DynBox, OCaml, OCamlBytes, OCamlInt, OCamlList, OCamlRef, ToOCaml,
+    ocaml_export, BoxRoot, DynBox, OCaml, OCamlBytes, OCamlFloat, OCamlInt, OCamlList, OCamlRef,
+    OCamlRuntime, ToOCaml,
 };
 use polars::prelude::*;
+use std::fmt::Display;
 use std::{borrow::Borrow, path::Path};
+
+fn ocaml_list_to_vec<T: 'static + Clone>(mut list: OCaml<OCamlList<DynBox<T>>>) -> Vec<T> {
+    let mut ret = Vec::new();
+
+    while let Some((head, tail)) = list.uncons() {
+        ret.push((*Borrow::<T>::borrow(&head)).clone());
+        list = tail;
+    }
+
+    ret
+}
+
+fn box_result<'a, T: 'static, E: Display>(
+    cr: &'a mut &'a mut OCamlRuntime,
+    result: Result<T, E>,
+) -> OCaml<'a, Result<DynBox<T>, String>> {
+    match result {
+        Err(err) => Err::<BoxRoot<DynBox<_>>, _>(err.to_string()).to_ocaml(cr),
+        Ok(v) => {
+            let v: BoxRoot<DynBox<T>> = OCaml::box_value(cr, v).root();
+            Ok::<_, String>(v).to_ocaml(cr)
+        }
+    }
+}
+
+fn expr_binary_op<'a>(
+    cr: &'a mut &'a mut OCamlRuntime,
+    expr: OCamlRef<'a, DynBox<Expr>>,
+    other: OCamlRef<'a, DynBox<Expr>>,
+    f: impl Fn(Expr, Expr) -> Expr,
+) -> OCaml<'a, DynBox<Expr>> {
+    let expr: Expr = Borrow::<Expr>::borrow(&expr.to_ocaml(cr)).clone();
+    let other: Expr = Borrow::<Expr>::borrow(&other.to_ocaml(cr)).clone();
+    OCaml::box_value(cr, f(expr, other))
+}
 
 // `ocaml_export` expands the function definitions by adding `pub` visibility and
 // the required `#[no_mangle]` and `extern` declarations. It also takes care of
 // acquiring the OCaml runtime handle and binding it to the name provided as
 // the first parameter of the function.
 ocaml_export! {
-    // The first parameter is a name to which the GC frame handle will be bound to.
-    // The remaining parameters must have type `OCamlRef<T>`, and the return
-    // value `OCaml<T>`.
-    fn rust_twice(cr, num: OCamlRef<OCamlInt>) -> OCaml<OCamlInt> {
-        let num: i64 = num.to_rust(cr);
-        unsafe { OCaml::of_i64_unchecked(num * 2) }
+    fn rust_expr_col(cr, name: OCamlRef<String>) -> OCaml<DynBox<Expr>> {
+        let name: String = name.to_rust(cr);
+        OCaml::box_value(cr, col(&name))
     }
 
-    fn rust_series_new(cr, name: OCamlRef<String>, values: OCamlRef<OCamlList<OCamlInt>>) -> OCaml<DynBox<Series>> {
+    fn rust_expr_int(cr, value: OCamlRef<OCamlInt>) -> OCaml<DynBox<Expr>> {
+        let value: i64 = value.to_rust(cr);
+        OCaml::box_value(cr, lit(value))
+    }
+
+    fn rust_expr_float(cr, value: OCamlRef<OCamlFloat>) -> OCaml<DynBox<Expr>> {
+        let value: f64 = value.to_rust(cr);
+        OCaml::box_value(cr, lit(value))
+    }
+
+    fn rust_expr_sort(cr, expr: OCamlRef<DynBox<Expr>>, descending: OCamlRef<bool>) -> OCaml<DynBox<Expr>> {
+        let expr: Expr = Borrow::<Expr>::borrow(&expr.to_ocaml(cr)).clone();
+        let descending: bool = descending.to_rust(cr);
+        OCaml::box_value(cr, expr.sort(descending))
+    }
+
+    fn rust_expr_head(cr, expr: OCamlRef<DynBox<Expr>>, length: OCamlRef<Option<OCamlInt>>) -> OCaml<Option<DynBox<Expr>>> {
+        let expr: Expr = Borrow::<Expr>::borrow(&expr.to_ocaml(cr)).clone();
+        let length: Option<i64> = length.to_rust(cr);
+
+        match length {
+            None => {
+                let expr: BoxRoot<DynBox<Expr>> = OCaml::box_value(cr, expr.head(None)).root();
+                Some(expr).to_ocaml(cr)
+            },
+            Some(length) => {
+                match length.try_into().ok() {
+                    // TODO: this should probably be an error instead of none
+                    None => OCaml::none(),
+                    Some(length) => {
+                        let expr: BoxRoot<DynBox<Expr>> = OCaml::box_value(cr, expr.head(Some(length))).root();
+                        Some(expr).to_ocaml(cr)
+                    },
+                }
+            }
+        }
+    }
+
+    fn rust_expr_filter(cr, expr: OCamlRef<DynBox<Expr>>, predicate: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        let expr: Expr = Borrow::<Expr>::borrow(&expr.to_ocaml(cr)).clone();
+        let predicate: Expr = Borrow::<Expr>::borrow(&predicate.to_ocaml(cr)).clone();
+        OCaml::box_value(cr, expr.filter(predicate))
+    }
+
+    fn rust_expr_sum(cr, expr: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        let expr: Expr = Borrow::<Expr>::borrow(&expr.to_ocaml(cr)).clone();
+        OCaml::box_value(cr, expr.sum())
+    }
+
+    fn rust_expr_alias(cr, expr: OCamlRef<DynBox<Expr>>, name: OCamlRef<String>) -> OCaml<DynBox<Expr>> {
+        let expr: Expr = Borrow::<Expr>::borrow(&expr.to_ocaml(cr)).clone();
+        let name: String = name.to_rust(cr);
+        OCaml::box_value(cr, expr.alias(&name))
+    }
+
+    fn rust_expr_eq(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a.eq(b))
+    }
+
+    fn rust_expr_neq(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a.neq(b))
+    }
+
+    fn rust_expr_gt(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a.gt(b))
+    }
+
+    fn rust_expr_gt_eq(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a.gt_eq(b))
+    }
+
+    fn rust_expr_lt(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a.lt(b))
+    }
+
+    fn rust_expr_lt_eq(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a.lt_eq(b))
+    }
+
+    fn rust_expr_not(cr, expr: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        let expr: Expr = Borrow::<Expr>::borrow(&expr.to_ocaml(cr)).clone();
+        OCaml::box_value(cr, expr.not())
+    }
+
+    fn rust_expr_and(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a.and(b))
+    }
+
+    fn rust_expr_or(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a.or(b))
+    }
+
+    fn rust_expr_xor(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a.xor(b))
+    }
+
+    fn rust_expr_add(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a + b)
+    }
+
+    fn rust_expr_sub(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a - b)
+    }
+
+    fn rust_expr_mul(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a * b)
+    }
+
+    fn rust_expr_div(cr, expr: OCamlRef<DynBox<Expr>>, other: OCamlRef<DynBox<Expr>>) -> OCaml<DynBox<Expr>> {
+        expr_binary_op(cr, expr, other, |a, b| a / b)
+    }
+
+    fn rust_series_new_int(cr, name: OCamlRef<String>, values: OCamlRef<OCamlList<OCamlInt>>) -> OCaml<DynBox<Series>> {
         let name: String = name.to_rust(cr);
         let values: Vec<i64> = values.to_rust(cr);
+        OCaml::box_value(cr, Series::new(&name, values))
+    }
+
+    fn rust_series_new_int_option(cr, name: OCamlRef<String>, values: OCamlRef<OCamlList<Option<OCamlInt>>>) -> OCaml<DynBox<Series>> {
+        let name: String = name.to_rust(cr);
+        let values: Vec<Option<i64>> = values.to_rust(cr);
+        OCaml::box_value(cr, Series::new(&name, values))
+    }
+
+    fn rust_series_new_float(cr, name: OCamlRef<String>, values: OCamlRef<OCamlList<OCamlFloat>>) -> OCaml<DynBox<Series>> {
+        let name: String = name.to_rust(cr);
+        let values: Vec<f64> = values.to_rust(cr);
+        OCaml::box_value(cr, Series::new(&name, values))
+    }
+
+    fn rust_series_new_float_option(cr, name: OCamlRef<String>, values: OCamlRef<OCamlList<Option<OCamlFloat>>>) -> OCaml<DynBox<Series>> {
+        let name: String = name.to_rust(cr);
+        let values: Vec<Option<f64>> = values.to_rust(cr);
+        OCaml::box_value(cr, Series::new(&name, values))
+    }
+
+    fn rust_series_new_string(cr, name: OCamlRef<String>, values: OCamlRef<OCamlList<String>>) -> OCaml<DynBox<Series>> {
+        let name: String = name.to_rust(cr);
+        let values: Vec<String> = values.to_rust(cr);
+        OCaml::box_value(cr, Series::new(&name, values))
+    }
+
+    fn rust_series_new_string_option(cr, name: OCamlRef<String>, values: OCamlRef<OCamlList<Option<String>>>) -> OCaml<DynBox<Series>> {
+        let name: String = name.to_rust(cr);
+        let values: Vec<Option<String>> = values.to_rust(cr);
         OCaml::box_value(cr, Series::new(&name, values))
     }
 
@@ -30,16 +206,9 @@ ocaml_export! {
     }
 
     fn rust_data_frame_new(cr, series: OCamlRef<OCamlList<DynBox<Series>>>) -> OCaml<Result<DynBox<DataFrame>,String>> {
-        let mut series: OCaml<OCamlList<DynBox<Series>>> = series.to_ocaml(cr);
+        let series = ocaml_list_to_vec(series.to_ocaml(cr));
 
-        let mut ret = Vec::new();
-        while let Some((head, tail)) = series.uncons() {
-            ret.push(Borrow::<Series>::borrow(&head).clone());
-
-            series = tail;
-        }
-
-        match DataFrame::new(ret) {
+        match DataFrame::new(series) {
             Err(err) => {
                 Err::<BoxRoot<DynBox<DataFrame>>, _>(err.to_string()).to_ocaml(cr)
             },
@@ -50,20 +219,24 @@ ocaml_export! {
         }
     }
 
+    fn rust_data_frame_to_string_hum(cr, data_frame: OCamlRef<DynBox<DataFrame>>) -> OCaml<String> {
+        let data_frame: OCaml<DynBox<DataFrame>> = data_frame.to_ocaml(cr);
+        let data_frame: &DataFrame = Borrow::<DataFrame>::borrow(&data_frame);
+        data_frame.to_string().to_ocaml(cr)
+    }
+
+    fn rust_data_frame_lazy(cr, data_frame: OCamlRef<DynBox<DataFrame>>) -> OCaml<DynBox<LazyFrame>> {
+        let data_frame: OCaml<DynBox<DataFrame>> = data_frame.to_ocaml(cr);
+        let data_frame: DataFrame = Borrow::<DataFrame>::borrow(&data_frame).clone();
+        OCaml::box_value(cr, data_frame.lazy())
+    }
+
     // TODO: properly return error type instead of a string
-    fn rust_lazy_frame_of_parquet(cr, path: OCamlRef<OCamlBytes>) -> OCaml<Result<DynBox<LazyFrame>, String>>{
+    fn rust_lazy_frame_scan_parquet(cr, path: OCamlRef<OCamlBytes>) -> OCaml<Result<DynBox<LazyFrame>, String>>{
         let path:String = path.to_rust(cr);
         let path:&Path = Path::new(&path);
 
-        match LazyFrame::scan_parquet(path, Default::default()) {
-            Err(err) => {
-                Err::<BoxRoot<DynBox<LazyFrame>>, _>(err.to_string()).to_ocaml(cr)
-            },
-            Ok(lazy_frame) => {
-                let lazy_frame: BoxRoot<DynBox<LazyFrame>> = OCaml::box_value(cr, lazy_frame).root();
-                Ok::<_, String>(lazy_frame).to_ocaml(cr)
-            }
-        }
+        box_result(cr, LazyFrame::scan_parquet(path, Default::default()))
     }
 
     fn rust_lazy_frame_to_dot(cr, lazy_frame: OCamlRef<DynBox<LazyFrame>>) -> OCaml<Result<String,String>>{
@@ -80,24 +253,20 @@ ocaml_export! {
         }
     }
 
-    fn rust_lazy_frame_to_data_frame(cr, lazy_frame: OCamlRef<DynBox<LazyFrame>>)-> OCaml<Result<DynBox<DataFrame>, String>> {
+    fn rust_lazy_frame_collect(cr, lazy_frame: OCamlRef<DynBox<LazyFrame>>)-> OCaml<Result<DynBox<DataFrame>, String>> {
         let lazy_frame: OCaml<DynBox<LazyFrame>> = lazy_frame.to_ocaml(cr);
+        let lazy_frame = Borrow::<LazyFrame>::borrow(&lazy_frame).clone();
 
-        match Borrow::<LazyFrame>::borrow(&lazy_frame).clone().collect() {
-            Err(err) => {
-                Err::<BoxRoot<DynBox<DataFrame>>, _>(err.to_string()).to_ocaml(cr)
-            },
-            Ok(data_frame) => {
-                let data_frame: BoxRoot<DynBox<DataFrame>> = OCaml::box_value(cr, data_frame).root();
-                Ok::<_, String>(data_frame).to_ocaml(cr)
-            }
-        }
+        box_result(cr, lazy_frame.collect())
     }
 
-    fn rust_data_frame_to_string_hum(cr, data_frame: OCamlRef<DynBox<DataFrame>>) -> OCaml<String> {
-        let data_frame: OCaml<DynBox<DataFrame>> = data_frame.to_ocaml(cr);
-        let data_frame: &DataFrame = Borrow::<DataFrame>::borrow(&data_frame);
-        data_frame.to_string().to_ocaml(cr)
+    fn rust_lazy_frame_select(cr, lazy_frame: OCamlRef<DynBox<LazyFrame>>, exprs: OCamlRef<OCamlList<DynBox<Expr>>>) -> OCaml<DynBox<LazyFrame>> {
+        let exprs = ocaml_list_to_vec(exprs.to_ocaml(cr));
+
+        let lazy_frame: OCaml<DynBox<LazyFrame>> = lazy_frame.to_ocaml(cr);
+        let lazy_frame:LazyFrame =  Borrow::<LazyFrame>::borrow(&lazy_frame).clone();
+
+        OCaml::box_value(cr, lazy_frame.select(&exprs))
     }
 }
 
