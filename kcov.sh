20af0c4922f4945ad102d9bc1c113605257c2c6a
Masayuki Takeda <mtakeda@oeconomia.jp>
2023-09-23 12:15:10 +0000

    don't parallelize code coverage collection when running in github actions

--- kcov.sh
+++ kcov.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -exo pipefail
 
 # We can't simply run `kcov dune runtest` since kcov seems to be unable to
 # properly track execution of the individual test executables. Instad, we
@@ -12,10 +12,19 @@ rm -rf $KCOV_DIR $KCOV_DIRS
 
 KCOV="kcov --include-pattern=polars-ocaml --exclude-pattern=ml-gen,test,guide --replace-src-path=/workspace_root:./"
 
-(cd _build/default && $KCOV /tmp/kcov_polars_async async/.polars_async.inline-tests/inline_test_runner_polars_async.exe inline-test-runner polars_async -source-tree-root . -diff-cmd -) &
-(cd _build/default && $KCOV /tmp/kcov_polars_guide guide/.polars_guide.inline-tests/inline_test_runner_polars_guide.exe inline-test-runner polars_guide -source-tree-root . -diff-cmd -) &
-(cd _build/default && $KCOV /tmp/kcov_polars lib/.polars.inline-tests/inline_test_runner_polars.exe inline-test-runner polars -source-tree-root . -diff-cmd -) &
-(cd _build/default && $KCOV /tmp/kcov_polars_tests test/.polars_tests.inline-tests/inline_test_runner_polars_tests.exe inline-test-runner polars_tests -source-tree-root . -diff-cmd -) &
-wait
+if [ "$CI" = "true" ]; then
+    # AFAICT there are some resource limits on GitHub Actions which prevent
+    # parallelization of code coverage collection, so we run these sequentially.
+    (cd _build/default && $KCOV /tmp/kcov_polars_async async/.polars_async.inline-tests/inline_test_runner_polars_async.exe inline-test-runner polars_async -source-tree-root . -diff-cmd -)
+    (cd _build/default && $KCOV /tmp/kcov_polars_guide guide/.polars_guide.inline-tests/inline_test_runner_polars_guide.exe inline-test-runner polars_guide -source-tree-root . -diff-cmd -)
+    (cd _build/default && $KCOV /tmp/kcov_polars lib/.polars.inline-tests/inline_test_runner_polars.exe inline-test-runner polars -source-tree-root . -diff-cmd -)
+    (cd _build/default && $KCOV /tmp/kcov_polars_tests test/.polars_tests.inline-tests/inline_test_runner_polars_tests.exe inline-test-runner polars_tests -source-tree-root . -diff-cmd -)
+else
+    (cd _build/default && $KCOV /tmp/kcov_polars_async async/.polars_async.inline-tests/inline_test_runner_polars_async.exe inline-test-runner polars_async -source-tree-root . -diff-cmd -) &
+    (cd _build/default && $KCOV /tmp/kcov_polars_guide guide/.polars_guide.inline-tests/inline_test_runner_polars_guide.exe inline-test-runner polars_guide -source-tree-root . -diff-cmd -) &
+    (cd _build/default && $KCOV /tmp/kcov_polars lib/.polars.inline-tests/inline_test_runner_polars.exe inline-test-runner polars -source-tree-root . -diff-cmd -) &
+    (cd _build/default && $KCOV /tmp/kcov_polars_tests test/.polars_tests.inline-tests/inline_test_runner_polars_tests.exe inline-test-runner polars_tests -source-tree-root . -diff-cmd -) &
+    wait
+fi
 
 kcov --merge $KCOV_DIR $KCOV_DIRS
