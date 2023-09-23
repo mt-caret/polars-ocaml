0866be112759fcfbcbb70033da6b36c9b34fbf3e
Masayuki Takeda <mtakeda@oeconomia.jp>
2023-09-23 10:25:22 +0000

    fix path treatment in kcov for ml file discovery

--- kcov.sh
+++ kcov.sh
@@ -13,9 +13,9 @@ dune runtest
 KCOV_DIR="/tmp/kcov"
 rm -rf "$KCOV_DIR"
 
-KCOV="kcov --include-pattern=polars-ocaml $KCOV_DIR"
+KCOV="kcov --include-pattern=polars-ocaml --exclude-pattern=ml-gen,test,guide --replace-src-path=/workspace_root:./ $KCOV_DIR"
 
-(cd _build/default/async && $KCOV .polars_async.inline-tests/inline_test_runner_polars_async.exe inline-test-runner polars_async -source-tree-root .. -diff-cmd -)
-(cd _build/default/guide && $KCOV .polars_guide.inline-tests/inline_test_runner_polars_guide.exe inline-test-runner polars_guide -source-tree-root .. -diff-cmd -)
-(cd _build/default/lib && $KCOV .polars.inline-tests/inline_test_runner_polars.exe inline-test-runner polars -source-tree-root .. -diff-cmd -)
-(cd _build/default/test && $KCOV .polars_tests.inline-tests/inline_test_runner_polars_tests.exe inline-test-runner polars_tests -source-tree-root .. -diff-cmd -)
+(cd _build/default && $KCOV async/.polars_async.inline-tests/inline_test_runner_polars_async.exe inline-test-runner polars_async -source-tree-root . -diff-cmd -)
+(cd _build/default && $KCOV guide/.polars_guide.inline-tests/inline_test_runner_polars_guide.exe inline-test-runner polars_guide -source-tree-root . -diff-cmd -)
+(cd _build/default && $KCOV lib/.polars.inline-tests/inline_test_runner_polars.exe inline-test-runner polars -source-tree-root . -diff-cmd -)
+(cd _build/default && $KCOV test/.polars_tests.inline-tests/inline_test_runner_polars_tests.exe inline-test-runner polars_tests -source-tree-root . -diff-cmd -)
