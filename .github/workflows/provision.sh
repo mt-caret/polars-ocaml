da7754d6d556b29a3abc1e986f6809c047cf9373
Masayuki Takeda <mtakeda@oeconomia.jp>
2023-09-30 12:35:30 +0000

    switch to provisioning base container instead of installing things from scratch

---
+++ .github/workflows/provision.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script contains the commands for provisioning the LXD container based on
+# the default ubuntu 22.04 image, to be used in tandem with the scripts in
+# https://github.com/stgraber/lxd-github-actions to set up self-hosted github
+# action runners on LXD.
+set -euxo pipefail
+
+sudo apt update
+# install opam + mold
+sudo apt install --yes \
+    curl \
+    opam \
+    mold
+opam init --auto-setup --compiler=4.14.1
+
+# install rustup, and make nightly toolchain default
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs |
+sh -s -- --default-toolchain=nightly
