7ee4f52e7a8d9e05a9a55a8a0fc9aa90102bc9d4
tapiocashapeaptitude <ozeng@janestreet.com>
2024-02-08 13:02:11 -0500

    Dune fmt

--- test/edit_dataframe_test.ml
+++ test/edit_dataframe_test.ml
@@ -2,12 +2,19 @@ open! Core
 open Polars
 
 let%expect_test "Basic modify -- float" =
-  let series = Series.float "float" [0.; 1.; 2.] in
-  let df = Data_frame.create_exn [series] in
+  let series = Series.float "float" [ 0.; 1.; 2. ] in
+  let df = Data_frame.create_exn [ series ] in
   Series.clear series;
-  Data_frame.Expert.modify_series_at_chunk_index df ~dtype:Float64 ~series_index:0 ~chunk_index:0 ~indices_and_values:[0, 10.0; 2, 20.0] |> Result.ok_or_failwith;
+  Data_frame.Expert.modify_series_at_chunk_index
+    df
+    ~dtype:Float64
+    ~series_index:0
+    ~chunk_index:0
+    ~indices_and_values:[ 0, 10.0; 2, 20.0 ]
+  |> Result.ok_or_failwith;
   Data_frame.to_string_hum df |> print_endline;
-  [%expect {|
+  [%expect
+    {|
     shape: (3, 1)
     ┌───────┐
     │ float │
@@ -21,12 +28,12 @@ let%expect_test "Basic modify -- float" =
 ;;
 
 let%expect_test "Basic modify -- float option" =
-  let series = Series.float "float" [0.; 1.; 2.] in
-  let series_option = Series.floato "float_option" [Some 0.; Some 1.; None] in
-  let series2 = Series.float "float" [0.; 1.; 2.] in
-  let series_option2 = Series.floato "float_option" [Some 0.; Some 1.; None] in
-  let df = Data_frame.create_exn [series; series_option] in
-  let chunk2 = Data_frame.create_exn [series2; series_option2] in
+  let series = Series.float "float" [ 0.; 1.; 2. ] in
+  let series_option = Series.floato "float_option" [ Some 0.; Some 1.; None ] in
+  let series2 = Series.float "float" [ 0.; 1.; 2. ] in
+  let series_option2 = Series.floato "float_option" [ Some 0.; Some 1.; None ] in
+  let df = Data_frame.create_exn [ series; series_option ] in
+  let chunk2 = Data_frame.create_exn [ series2; series_option2 ] in
   Data_frame.vstack_exn df ~other:chunk2;
   Series.clear series;
   Series.clear series_option;
@@ -37,12 +44,16 @@ let%expect_test "Basic modify -- float option" =
   Series.Expert.compute_null_count series |> Int.to_string |> print_endline;
   Series.clear series;
   [%expect {| 2 |}];
-  Data_frame.Expert.modify_optional_series_at_chunk_index df ~dtype:Float64
-    ~series_index:1 ~chunk_index:0
-    ~indices_and_values:[0, None; 1, None; 2, Some 20.0]
+  Data_frame.Expert.modify_optional_series_at_chunk_index
+    df
+    ~dtype:Float64
+    ~series_index:1
+    ~chunk_index:0
+    ~indices_and_values:[ 0, None; 1, None; 2, Some 20.0 ]
   |> Result.ok_or_failwith;
   Data_frame.to_string_hum df |> print_endline;
-  [%expect {|
+  [%expect
+    {|
     shape: (6, 2)
     ┌───────┬──────────────┐
     │ float ┆ float_option │
@@ -57,6 +68,8 @@ let%expect_test "Basic modify -- float option" =
     │ 2.0   ┆ null         │
     └───────┴──────────────┘ |}];
   Data_frame.column_exn df ~name:"float_option"
-  |> Series.Expert.compute_null_count |> Int.to_string |> print_endline;
+  |> Series.Expert.compute_null_count
+  |> Int.to_string
+  |> print_endline;
   [%expect {| 3 |}]
 ;;
